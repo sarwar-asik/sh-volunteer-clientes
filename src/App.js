42d7bae0f3a74ab8b710c26fc5982664b0826a1a
sarwar-asik <shasik2021@gmail.com>
2022-11-07 10:16:07 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UploafImg from './components/uploadimg/UploafImg';
+
+function App() {
+  return (
+    <div className="App">
+     <UploafImg></UploafImg>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/uploadimg/UploafImg.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const UploafImg = () => {
+  return (
+    <div>
+      <button className="btn btn-outline btn-warning my-5  p-5">
+        {" "}
+        <a href="https://imgbb.com/upload">Upload </a>
+      </button>
+
+      <img src="https://i.ibb.co/ydCbDN3/5555.jpg" alt="" />
+      <br />
+      <img src="https://ibb.co/WWhmydN" alt="" />
+      <a href="https://imgbb.com/"><img src="" alt="nowaf-jpg-Copy" border="0"/></a>
+
+      <br />
+
+      <img src='https://i.ibb.co/GC1Z8xb/nowaf-jpg-Copy.png' alt="nowaf-jpg-Cop" />
+      
+    </div>
+  );
+};
+
+export default UploafImg;
